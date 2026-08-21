bf04eadeecadc64939549c508e488bbdb1cccd5d
johnabrams7 <johnlangsto@gmail.com>
2026-08-20 20:17:27 -0500

    styles, verbiage, cleanup

--- js/script.js
+++ js/script.js
@@ -1,45 +1,5 @@
 $(document).ready(function () {
 
-  
-  // Jquery smooth scrolling 
-  // $('a[href*="#"]')
-  // // Remove links that don't actually link to anything
-  // .not('[href="#"]')
-  // .not('[href="#0"]')
-  // .click(function(event) {
-  //   // On-page links
-  //   if (
-  //     location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-  //     && 
-  //     location.hostname == this.hostname
-  //     &&
-  //     $(window).width() > 768
-  //   ) {
-  //     // Figure out element to scroll to
-  //     var target = $(this.hash);
-  //     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-  //     // Does a scroll target exist?
-  //     if (target.length) {
-  //       // Only prevent default if animation is actually gonna happen
-  //       event.preventDefault();
-  //       $('html, body').animate({
-  //         scrollTop: target.offset().top
-  //       }, 800, function() {
-  //         // Callback after animation
-  //         // Must change focus!
-  //         var $target = $(target);
-  //         $target.focus();
-  //         if ($target.is(":focus")) { // Checking if the target was focused
-  //           return false;
-  //         } else {
-  //           $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-  //           $target.focus(); // Set focus again
-  //         };
-  //       });
-  //     }
-  //   }
-  // });
-
   $(window).scroll(function () {
     // sticky navbar on scroll script
     if (this.scrollY > 20) {
@@ -79,8 +39,7 @@ $(document).ready(function () {
     strings: [
       "Front-End Developer",
       "UX Designer",
-      "Technician",
-      "Media Producer",
+      "Technician"
     ],
     typeSpeed: 100,
     backSpeed: 60,
@@ -100,8 +59,7 @@ $(document).ready(function () {
     strings: [
       "Front-End Developer",
       "UX Designer",
-      "Technician",
-      "Media Producer",
+      "Technician"
     ],
     typeSpeed: 100,
     backSpeed: 60,
